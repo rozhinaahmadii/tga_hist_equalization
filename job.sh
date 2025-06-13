af0fb124aca364f2c02a7453d706d35add4eae55
Jan Anton Villanueva <163298637+jantonvi6@users.noreply.github.com>
2025-06-13 12:31:55 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -9,24 +9,23 @@
 #SBATCH -p cuda
 
 ## SOLO 1 DE LAS TRES OPCIONES PUEDE ESTAR ACTIVA
-## OPCION A: Usamos la RTX 4090
-##SBATCH --qos=cuda4090  
-##SBATCH --gres=gpu:rtx4090:1
 
-## OPCION B: Usamos las 4 RTX 3080
-##SBATCH --qos=cuda3080  
-##SBATCH --gres=gpu:rtx3080:4
-
-## OPCION C: Usamos 1 RTX 3080
+## OPCION A: Usamos 1 RTX 3080
 #SBATCH --qos=cuda3080  
 #SBATCH --gres=gpu:rtx3080:1
+
+## OPCION B: Usamos 2 RTX 3080
+##SBATCH --qos=cuda3080  
+##SBATCH --gres=gpu:rtx3080:2
+
+## OPCION C: Usamos 4 RTX 3080
+##SBATCH --qos=cuda3080  
+#3SBATCH --gres=gpu:rtx3080:4
 
 export PATH=/Soft/cuda/12.2.2/bin:$PATH
 
 ./Hist-CPU.exe 
-##./SaxpyP.exe 
-##./SaxpyP.exe 
-##./SaxpyP.exe 
+
 
 #echo "PRINT GPU SUMMARY nsys nvprof --print-gpu-summary"
 #echo "===================================================================="
@@ -38,21 +37,6 @@ export PATH=/Soft/cuda/12.2.2/bin:$PATH
 #nsys nvprof --print-gpu-trace ./SaxpyP.exe
 #echo "===================================================================="
 
-#./SaxpyP.exe 16777216 32 
-#./SaxpyP.exe 16777216 64
-#./SaxpyP.exe 16777216 128
-#./SaxpyP.exe 16777216 256
-#./SaxpyP.exe 16777216 512
-#./SaxpyP.exe 16777216 1024
-
-## 1024+32
-##./SaxpyP.exe 16777216 1056
-
-#./SaxpyP.exe 16777216 50 
-#./SaxpyP.exe 16777216 100
-#./SaxpyP.exe 16777216 200
-#./SaxpyP.exe 16777216 500
-#./SaxpyP.exe 16777216 1000
 
 
 
